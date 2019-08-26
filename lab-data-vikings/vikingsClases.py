de4ea4fc1f9584376378212308cbee17b9886620
Fadrique Kindler <fakindler@hotmail.com>
2019-08-26 16:18:54 +0200

    Tests not working

--- lab-data-vikings/vikingsClases.py
+++ lab-data-vikings/vikingsClases.py
@@ -3,7 +3,21 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        if self.attack:
+            return strenght
+        
+    
+    def recieveDamage(self, Damage):
+        if self.Damage:
+            return (health - Damage)
+        
+
+
 
 # Viking
 
