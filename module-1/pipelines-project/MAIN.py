14b9f8849fad2b5302b42e1cf0f9b7f62c000ec1
Fadrique Kindler <fakindler@hotmail.com>
2019-09-16 15:24:15 +0200

    Finalizing Pipeline

--- module-1/pipelines-project/MAIN.py
+++ module-1/pipelines-project/MAIN.py
@@ -1,28 +1,28 @@
-from Functions import , get_players, clean_df_players, get_player_data, wrangle_df_player_data, get_season_stats
+from Functions import get_players, clean_df_players, get_player_data, wrangle_df_player_data, get_season_stats
 
 from outputs import print_file
 
-def main(): 
-    mm = MetroLineas()
-    mm.scan()
+def main():
+    df_players = get_players()
+    filtered_players = clean_df_players(df_players)
+    df_player_data = get_player_data()
+    filtered_player_data = clean_df_players(df_player_data)
+    df_season_stats = get_season_stats()
+    filtered_df_season_stats = clean_df_season_stats(df_season_stats)
+    complete_df_merge = merging_dfs(filtered_players,filtered_player_data,filtered_df_season_stats)
+    filtered_complete_df = wrangle_complete_df(complete_df_merge)
+    return filtered_complete_df
 
-    urls = mm.urls_lineas
-
-    metro = {}
-    for url in urls: 
-        l = Linea(url)
-        l.scan()
-        metro[l.name] = l.paradas
-    print(metro)
-    print_file(metro)
-
- 
-    
-
-
-
-    
-
-
-if __name__ == "__main__": 
+if __name__ == '__main__':
     main()
+    
+
+    '''
+    
+    max_pob = analyze_1(filtered)
+    chart = visualize_1(max_pob)
+    save_viz_3(chart, 'Most populated countries')
+    pctchange = analyze_2(filtered)
+    chart_pct = visualize_2(pctchange)
+    save_viz_3(chart_pct, 'Population Percentage Change')
+    '''
